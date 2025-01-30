5d5b2e2b5aae71f27845ac185e4fc0801cb2c1e6
Selina Tran <67066982+selinat3612@users.noreply.github.com>
2025-01-30 14:14:29 -0500

    Added instructions page

---
+++ src/public/demo-mqp-study/assets/textInstructionVideo.tsx
@@ -0,0 +1,19 @@
+const InstructionVideo: React.FC = function InstructionVideo() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingBottom: '4%',
+      }}
+    >
+      <video width="1000" controls>
+        <source src="/demo-mqp-study/assets/text_instruction.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+    </div>
+  );
+};
+
+export default InstructionVideo;
