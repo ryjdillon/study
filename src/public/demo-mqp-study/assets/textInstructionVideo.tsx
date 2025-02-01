1a689a40e5b44c141ec9cc3782d7b8a506a6f63e
kylieflerlage <kgflerlage@wpi.edu>
2025-02-01 15:05:00 -0500

    Update textInstructionVideo.tsx
    simple update to remove stray text

--- src/public/demo-mqp-study/assets/textInstructionVideo.tsx
+++ src/public/demo-mqp-study/assets/textInstructionVideo.tsx
@@ -8,7 +8,6 @@ const InstructionVideo: React.FC = function InstructionVideo() {
         paddingBottom: '4%',
       }}
     >
-      public/demo-mqp-study/assets/pie_instruction.mp4
       <video width="1000" controls>
         <source src="./assets/text_instruction.mp4" type="video/mp4" />
         Your browser does not support the video tag.
