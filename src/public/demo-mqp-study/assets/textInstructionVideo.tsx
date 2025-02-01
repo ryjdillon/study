ec4933a97875766f4a3167e7959a0339c61007d9
kylieflerlage <kgflerlage@wpi.edu>
2025-02-01 15:01:44 -0500

    video fix 2
    trying to link videos correctly try 2

--- src/public/demo-mqp-study/assets/textInstructionVideo.tsx
+++ src/public/demo-mqp-study/assets/textInstructionVideo.tsx
@@ -10,7 +10,7 @@ const InstructionVideo: React.FC = function InstructionVideo() {
     >
       public/demo-mqp-study/assets/pie_instruction.mp4
       <video width="1000" controls>
-        <source src="/public/demo-mqp-study/assets/text_instruction.mp4" type="video/mp4" />
+        <source src="./assets/text_instruction.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
